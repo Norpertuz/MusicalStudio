08ae5159678cadf7e6f476c653a8655a1037d5bc
Norbert Zborecki <norbertzborecki@gmail.com>
2019-10-23 17:23:00 +0200

    fix: grid

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -5,14 +5,14 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.GridLayout;
+
 public class MainWindow {
 
 	private JFrame frame;
 	private JTextField txtSearch;
+	private JPanel listPanel;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -26,31 +26,26 @@ public class MainWindow {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public MainWindow() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setSize(1200, 800);
+		frame.setSize(1000, 700);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel navigationPanel = new JPanel();
 		navigationPanel.setBackground(Color.BLUE);
-		navigationPanel.setBounds(0, 0, 100, 778);
+		navigationPanel.setBounds(0, 0, 80, 678);
 		frame.getContentPane().add(navigationPanel);
 		navigationPanel.setLayout(null);
 		
 		JButton homeButton = new JButton("home");
-		homeButton.setBounds(9, 177, 85, 29);
+		homeButton.setBounds(0, 164, 85, 29);
 		navigationPanel.add(homeButton);
 		
 		JButton favButton = new JButton("playlists");
@@ -75,7 +70,7 @@ public class MainWindow {
 		
 		JPanel topPanel = new JPanel();
 		topPanel.setBackground(Color.WHITE);
-		topPanel.setBounds(100, 0, 1100, 80);
+		topPanel.setBounds(80, 0, 920, 80);
 		frame.getContentPane().add(topPanel);
 		topPanel.setLayout(null);
 		
@@ -91,20 +86,21 @@ public class MainWindow {
 		listButton.setBounds(518, 28, 84, 29);
 		topPanel.add(listButton);
 		
-		JPanel listPanel = new JPanel();
+		listPanel = new JPanel();
 		listPanel.setBackground(Color.WHITE);
-		listPanel.setBounds(112, 92, 736, 669);
+		listPanel.setBounds(90, 90, 554, 577);
 		frame.getContentPane().add(listPanel);
 		
 		JPanel infoPanel = new JPanel();
 		infoPanel.setBackground(Color.WHITE);
-		infoPanel.setBounds(860, 162, 334, 599);
+		infoPanel.setBounds(656, 160, 334, 507);
 		frame.getContentPane().add(infoPanel);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(860, 92, 334, 58);
+		panel.setBounds(656, 90, 334, 58);
 		frame.getContentPane().add(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		txtSearch = new JTextField();
 		txtSearch.setText("Search");

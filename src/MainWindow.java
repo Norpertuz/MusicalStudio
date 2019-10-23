d09e75a11826989f21fd543288d5f7790134f892
Norbert Zborecki <norbertzborecki@gmail.com>
2019-10-23 18:53:40 +0200

    add: heading

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -15,11 +15,12 @@ import java.awt.Component;
 import javax.swing.SwingConstants;
 import java.awt.Dimension;
 import javax.swing.JTextArea;
+import java.awt.Font;
 
 public class MainWindow {
 
 	private JFrame frame;
-	private JTextField txtSearch;
+	private JTextField searchBar;
 	private JPanel listPanel;
 
 	public static void main(String[] args) {
@@ -42,14 +43,14 @@ public class MainWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setSize(1000, 700);
+		frame.setSize(1000, 750);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel navigationPanel = new JPanel();
 		navigationPanel.setBackground(Color.BLUE);
-		navigationPanel.setBounds(0, 0, 80, 678);
+		navigationPanel.setBounds(0, 0, 80, 728);
 		frame.getContentPane().add(navigationPanel);
 		navigationPanel.setLayout(null);
 		
@@ -97,82 +98,118 @@ public class MainWindow {
 		
 		listPanel = new JPanel();
 		listPanel.setBackground(Color.WHITE);
-		listPanel.setBounds(90, 90, 608, 577);
+		listPanel.setBounds(90, 90, 608, 628);
 		frame.getContentPane().add(listPanel);
+		listPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(15, 15, 579, 61);
+		listPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel PlaylistHeading = new JLabel("PLAYLIST");
+		PlaylistHeading.setBounds(0, 0, 89, 25);
+		PlaylistHeading.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		panel.add(PlaylistHeading);
+		
+		JPanel headingLine = new JPanel();
+		headingLine.setBackground(Color.BLUE);
+		headingLine.setBounds(0, 30, 48, 4);
+		panel.add(headingLine);
 		
 		JPanel infoPanel = new JPanel();
 		infoPanel.setBackground(Color.WHITE);
-		infoPanel.setBounds(710, 148, 280, 519);
+		infoPanel.setBounds(710, 148, 280, 570);
 		frame.getContentPane().add(infoPanel);
 		infoPanel.setLayout(null);
 		
 		JLabel albumImage = new JLabel("avatar");
 		albumImage.setOpaque(true);
 		albumImage.setBackground(Color.ORANGE);
-		albumImage.setBounds(10, 10, 260, 260);
+		albumImage.setBounds(15, 70, 250, 250);
 		infoPanel.add(albumImage);
 		
 		JLabel albumTitle = new JLabel("BELLA CIAO");
-		albumTitle.setBounds(10, 280, 260, 16);
+		albumTitle.setBounds(15, 335, 250, 16);
 		infoPanel.add(albumTitle);
 		
 		JLabel albumAuthor = new JLabel("Sergio y Andres");
-		albumAuthor.setBounds(10, 300, 260, 16);
+		albumAuthor.setForeground(Color.BLUE);
+		albumAuthor.setBounds(15, 355, 250, 16);
 		infoPanel.add(albumAuthor);
 		
 		JLabel albumAlbum = new JLabel("Album:");
-		albumAlbum.setBounds(10, 340, 50, 16);
+		albumAlbum.setBounds(15, 395, 50, 16);
 		infoPanel.add(albumAlbum);
 		
 		JLabel albumDuration = new JLabel("Duration:");
-		albumDuration.setBounds(10, 360, 84, 16);
+		albumDuration.setBounds(15, 415, 59, 16);
 		infoPanel.add(albumDuration);
 		
 		JLabel albumMusicGenre = new JLabel("Music genre:");
-		albumMusicGenre.setBounds(10, 380, 84, 16);
+		albumMusicGenre.setBounds(15, 435, 84, 16);
 		infoPanel.add(albumMusicGenre);
 		
 		JLabel albumReleaseDate = new JLabel("Release date:");
-		albumReleaseDate.setBounds(10, 400, 84, 16);
+		albumReleaseDate.setBounds(15, 455, 84, 16);
 		infoPanel.add(albumReleaseDate);
 		
 		JLabel albumDescription = new JLabel("Description:");
-		albumDescription.setBounds(10, 420, 84, 16);
+		albumDescription.setBounds(15, 475, 84, 16);
 		infoPanel.add(albumDescription);
 		
 		JLabel dataAlbum = new JLabel("La Casa de Papel - Soundtrack");
-		dataAlbum.setBounds(62, 340, 208, 16);
+		dataAlbum.setForeground(Color.LIGHT_GRAY);
+		dataAlbum.setBounds(68, 395, 198, 16);
 		infoPanel.add(dataAlbum);
 		
 		JLabel dataTime = new JLabel("4:25");
-		dataTime.setBounds(75, 360, 195, 16);
+		dataTime.setForeground(Color.LIGHT_GRAY);
+		dataTime.setBounds(80, 415, 185, 16);
 		infoPanel.add(dataTime);
 		
 		JLabel dataMusicGenre = new JLabel("Pop");
-		dataMusicGenre.setBounds(96, 380, 174, 16);
+		dataMusicGenre.setForeground(Color.LIGHT_GRAY);
+		dataMusicGenre.setBounds(102, 435, 164, 16);
 		infoPanel.add(dataMusicGenre);
 		
 		JLabel dataReleaseDate = new JLabel("01 January 2018");
-		dataReleaseDate.setBounds(96, 400, 174, 16);
+		dataReleaseDate.setForeground(Color.LIGHT_GRAY);
+		dataReleaseDate.setBounds(102, 455, 164, 16);
 		infoPanel.add(dataReleaseDate);
 		
 		JTextArea dataDescription = new JTextArea();
+		dataDescription.setForeground(Color.LIGHT_GRAY);
 		dataDescription.setWrapStyleWord(true);
 		dataDescription.setLineWrap(true);
-		dataDescription.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.");
+		dataDescription.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ...");
 		dataDescription.setEditable(false);
-		dataDescription.setBounds(10, 440, 260, 69);
+		dataDescription.setBounds(15, 495, 250, 69);
 		infoPanel.add(dataDescription);
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(710, 90, 280, 46);
-		frame.getContentPane().add(panel);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		JLabel label = new JLabel("PLAYLIST");
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label.setBounds(15, 10, 89, 25);
+		infoPanel.add(label);
 		
-		txtSearch = new JTextField();
-		txtSearch.setText("Search");
-		panel.add(txtSearch);
-		txtSearch.setColumns(10);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.BLUE);
+		panel_1.setBounds(15, 40, 48, 4);
+		infoPanel.add(panel_1);
+		
+		JPanel searchPanel = new JPanel();
+		searchPanel.setBackground(Color.WHITE);
+		searchPanel.setBounds(710, 90, 280, 46);
+		frame.getContentPane().add(searchPanel);
+		searchPanel.setLayout(null);
+		
+		searchBar = new JTextField();
+		searchBar.setBorder(null);
+		searchBar.setForeground(Color.LIGHT_GRAY);
+		searchBar.setCaretColor(Color.BLUE);
+		searchBar.setBounds(44, 0, 236, 46);
+		searchBar.setText("Search");
+		searchPanel.add(searchBar);
+		searchBar.setColumns(10);
 	}
 }

71c5851f1d31cc6f1b2aa2fc838e2ad471b33330
Norbert Zborecki <norbertzborecki@gmail.com>
2019-11-13 12:19:30 +0100

    fix: focus

--- src/Colors.java
+++ src/Colors.java
@@ -1,10 +1,10 @@
 import java.awt.Color;
 
 public class Colors {
-	Color inactive = Color.decode("#4C506D");
-	Color active = Color.WHITE;
+	static Color inactive = Color.decode("#4C506D");
+	static Color active = Color.WHITE;
 	
 	//dark theme
 	static Color darkThemeRed = Color.decode("#EE2B47");
-	Color darkThemeBackground = Color.decode("#2C2E3E");
+	static Color darkThemeBackground = Color.decode("#2C2E3E");
 }
--- src/TextField.java
+++ src/TextField.java
@@ -1,11 +1,15 @@
-import java.awt.Color;
-
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
 
 public class TextField extends JTextField {
+	private MatteBorder active = BorderFactory.createMatteBorder(3,3,3,3, Colors.active);
+	private MatteBorder inactive = BorderFactory.createMatteBorder(3,3,3,3, Colors.inactive);
 	
 	@Override
 	public void setBorder(Border border) {
@@ -15,8 +19,18 @@ public class TextField extends JTextField {
 	public TextField() {
 		this.setVisible(true);
 		this.setBackground(null);
-		
-		MatteBorder active = BorderFactory.createMatteBorder(3,3,3,3, Colors.darkThemeRed);
-		this.setBorder(active);
+		// focus --> START
+		this.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				((JComponent) e.getComponent()).setBorder(active);	
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				((JComponent) e.getComponent()).setBorder(inactive);	
+			}
+			
+		});
+		// focus --> END
 	}
 }

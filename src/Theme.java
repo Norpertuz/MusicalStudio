6603946ed555b181976dc248ddc3853e9d1eeb17
Norbert Zborecki <norbertzborecki@gmail.com>
2019-11-22 22:44:22 +0100

    fix a duplicate process

--- src/Theme.java
+++ src/Theme.java
@@ -1,12 +1,14 @@
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Theme extends JPanel{
-	public Theme(boolean dark) {
-		if (dark == true) {
-			this.setBackground(Colors.DTBackground);
+	public Theme(boolean dark, JFrame frame) {
+		String Parent = frame.getClass().getName();
+		if (Parent == "Login") {
+			System.out.println(Parent);
 		} else {
-			this.setBackground(Colors.LTPanel);
+			System.out.println(Parent);
 		}
 	}
 }
